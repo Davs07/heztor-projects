3706ec881c7d9e70862b635ab837659f02cee74e
Davs07 <botsymeisterspieler@gmail.com>
2024-05-23 19:36:03 -0500

    Updated ProjectCard component to allow editing of project names, added onUpdateName callback to Projects page, and modified ProjectCard usage in Projects page to include onUpdateName prop.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,16 +1,81 @@
-import { Project } from "@/utils/types";
-import { Card, CardHeader } from "./ui/card";
+import { Id, Project } from "@/utils/types";
+import { Card, CardContent, CardHeader } from "./ui/card";
+import { Button } from "./ui/button";
+import { Ellipsis } from "lucide-react";
+import { Textarea } from "./ui/textarea";
+import { useEffect, useRef, useState } from "react";
 
 interface Props {
   project: Project;
+  onUpdateName: (id: Id, name: string) => void;
   //Project has id: Id;  name: string;   description?: string;   status: boolean;
 }
 
-export const ProjectCard = ({ project }: Props) => {
+export const ProjectCard = ({ project, onUpdateName }: Props) => {
+  const [editMode, setEditMode] = useState(false);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const toggleEditMode = () => setEditMode((prev) => !prev);
+  const disableEditMode = () => setEditMode(false);
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      disableEditMode();
+    }
+  };
+
+  const handleBlur = () => {
+    if (editMode) {
+      disableEditMode();
+    }
+  };
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "28px";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [editMode]);
   return (
-    <Card className=" px-2 w-[300px] h-[80px] rounded-2xl border-border border">
-      <CardHeader>
-        <p>{project.name}</p>
+    <Card className=" px-2 w-[300px] min-h-[80px] h-max max-h-max rounded-2xl border-border border group">
+      <CardHeader className="flex flex-row w-full h-max justify-between items-center gap-3 ">
+        <div className="w-full">
+          {editMode ? (
+            <Textarea
+              ref={textareaRef}
+              className="text-md min-h-4 w-full  px-1 py-0 h-auto resize-none overflow-hidden"
+              defaultValue={project.name}
+              onChange={(e) => {
+                onUpdateName(project.id, e.target.value);
+                if (textareaRef.current) {
+                  textareaRef.current.style.height = "28px";
+                  textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+                }
+              }}
+              onKeyDown={handleKeyDown}
+              onBlur={handleBlur}
+              autoFocus
+              placeholder="Agrega una tarea"
+            />
+          ) : (
+            <h4
+              className="text-md w-full max-w-full"
+              onClick={editMode ? undefined : toggleEditMode}>
+              {project.name}
+            </h4>
+          )}
+        </div>
+
+        <Button
+          variant={"none2"}
+          size={"icon2"}
+          className="rounded-lg opacity-0 group-hover:opacity-100 "
+          onClick={(e) => {
+            e.stopPropagation();
+          }}>
+          <Ellipsis />
+        </Button>
       </CardHeader>
     </Card>
   );
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,13 +4,34 @@ import { Project } from "@/utils/types";
 import { ProjectCard } from "@/components/ProjectCard";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { generateId } from "@/utils/utilsProjects";
 
 export const Projects = () => {
   const [projects, setProjects] = useState<Project[]>(initialProjects);
 
   const onCreateProject = () => {
+    const newProject = {
+      id: generateId(),
+      name: "New Project",
+      status: false,
+    };
 
-  }
+    setProjects([...projects, newProject]);
+  };
+
+  const onUpdateName = (id: Project["id"], name: Project["name"]) => {
+    const project = projects.find((project) => project.id === id);
+
+    if (project) {
+      const updatedProjects = projects.map((project) => {
+        if (project.id === id) {
+          return { ...project, name };
+        }
+        return project;
+      });
+      setProjects(updatedProjects);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center w-full min-h-screen h-screen max-h-screen py-2 px-4 gap-6">
@@ -19,9 +40,15 @@ export const Projects = () => {
       <div className="flex justify-center w-full">
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4  ">
           {projects.map((project) => (
-            <ProjectCard key={project.id} project={project} />
+            <ProjectCard
+              key={project.id}
+              project={project}
+              onUpdateName={onUpdateName}
+            />
           ))}
-          <Button className="flex items-center gap-2 w-[300px] h-[80px] px-2 bg-transparent shadow-none rounded-2xl hover:bg-gray-100 text-gray-400 hover:text-primary">
+          <Button
+            className="flex items-center gap-2 w-[300px] h-[80px] px-2 bg-transparent shadow-none rounded-2xl hover:bg-gray-100 text-gray-400 hover:text-primary"
+            onClick={onCreateProject}>
             <Plus />
             <p>Añade un nuevo proyecto</p>
           </Button>
